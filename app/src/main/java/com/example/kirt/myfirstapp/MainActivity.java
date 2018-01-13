cdf95e8a1e957d8b13d68fdf287738351661640b
Kirt Russell Tan <russelltan65@gmail.com>
2018-01-13 23:21:44 +0800

    Add 2 numbers

--- app/src/main/java/com/example/kirt/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/kirt/myfirstapp/MainActivity.java
@@ -2,45 +2,24 @@ package com.example.kirt.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String MY_TAG = "my custom message";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(MY_TAG,"onCreate invoked");
     }
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("lifecycle","onStart invoked");
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("lifecycle","onResume invoked");
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("lifecycle","onPause invoked");
-    }
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("lifecycle","onStop invoked");
-    }
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d("lifecycle","onRestart invoked");
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("lifecycle","onDestroy invoked");
+    public void onButtonClick(View v) {
+        EditText e1 =(EditText)findViewById(R.id.num1);
+        EditText e2 =(EditText)findViewById(R.id.num2);
+        TextView t1 = (TextView)findViewById(R.id.result);
+        int num1 = Integer.parseInt(e1.getText().toString());
+        int num2 = Integer.parseInt(e2.getText().toString());
+        int sum = num1 + num2;
+        t1 .setText(Integer.toString(sum));
     }
 }
