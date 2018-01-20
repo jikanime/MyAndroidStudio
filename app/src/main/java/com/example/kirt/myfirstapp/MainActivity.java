f417c8a7a3e0a3b4294c5242382759daf517bb0a
Kirt Russell Tan <russelltan65@gmail.com>
2018-01-20 21:57:53 +0800

    Toast

--- app/src/main/java/com/example/kirt/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/kirt/myfirstapp/MainActivity.java
@@ -5,18 +5,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private EditText pass_word;
+    private Button button;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onButtonClick();
     }
-    public void onButtonClick(View v) {
-        EditText e1 =(EditText)findViewById(R.id.input);
-        TextView t1 = (TextView)findViewById(R.id.result);
-        String input = e1.getText().toString();
-        t1.setText(input);
+    public void onButtonClick()
+    {
+        pass_word = (EditText)findViewById(R.id.input);
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View w)
+                {
+                    Toast.makeText(
+                        MainActivity.this, pass_word.getText(), Toast.LENGTH_SHORT
+                    ).show();
+                }
+            }
+        );
     }
 }
